42327acfce1673eaf445a70eedb070837510e98f
Johnny Chen <johnnychen94@hotmail.com>
2021-04-22 11:23:38 +0800

    upgrade to ImageCore 0.9
    remove direct dependency to ColorVectorSpace 0.9

--- src/ImageMorphology.jl
+++ src/ImageMorphology.jl
@@ -3,7 +3,6 @@ module ImageMorphology
 using ImageCore
 using ImageCore: GenericGrayImage
 using LinearAlgebra
-using ColorVectorSpace
 using Base.Cartesian # TODO: delete this
 using TiledIteration: EdgeIterator
 
