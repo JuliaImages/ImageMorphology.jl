30097baa07319750a839b7a878018de58dd78fbd
Retornaz Thomas <thomas.retornaz@mines-paris.org>
2022-04-05 18:11:20 +0200

    [Morphology][Geodesy] Back to basics the 1993 algorithms is far fatser than the one which use priority_queue

--- src/geodesy.jl
+++ src/geodesy.jl
@@ -9,7 +9,7 @@ end
 
 import Base.isless
 import Base.isgreater
-import DataStructures.PriorityQueue, DataStructures.Queue, DataStructures.Deque, DataStructures.enqueue!, DataStructures.dequeue!, DataStructures.dequeue_pair!
+import DataStructures.Queue, DataStructures.Deque, DataStructures.enqueue!, DataStructures.dequeue!, DataStructures.dequeue_pair!
 
 
 """
@@ -72,9 +72,8 @@ An array of the same type and shape as the `marker`.
 function underbuild(marker::AbstractArray{T, N}, mask::AbstractArray{T, N}, connectivity::AbstractArray{Bool}) where {T<:ImageCore.NumberLike , N}
     check_image_consistency(mask, marker)
     # WARNING
-    #  The original algorithm compute regional_maxima on the Marker
-    #  But this algorithm assume that the marker < mask, this assertion is not always true
-    #  so instead we extract the regional_maxima from min(marker,mask)
+    #  The original algorithm assume that the marker < mask, this assertion is not always true
+    #  so instead we extract the start from min(marker,mask)
    
     all(in((1,3)), size(connectivity)) || throw(ArgumentError("connectivity must have size 1 or 3 in each dimension"))
     for d = 1:ndims(connectivity)
@@ -83,40 +82,53 @@ function underbuild(marker::AbstractArray{T, N}, mask::AbstractArray{T, N}, conn
    
     # first operation take the infimum between marker and mask
     output = map(min, marker, mask)
-    # then compute regional_maxima of the Inf(Mask,Marker)
-    rmaxima = regional_maxima(output, connectivity)
-    # declare priority queue, store position of pixel associated with priority 
-    pq = PriorityQueue{CartesianIndex{N}, T}(Base.Order.Reverse)
+
+    queue = Queue{CartesianIndex{N}}(); 
     deltaoffsets = neighborhood(marker, connectivity)
-    visited = similar(Array{Bool}, axes(marker))
-    fill!(visited, false)
-    # all regional maxima are pushed in priority queue
+    uper_deltaoffest = upper_half_neighborhood(marker, connectivity)
+    lower_deltaoffest= lower_half_neighborhood(marker, connectivity)
+
+    # forward scan
     R = CartesianIndices(axes(marker))
     for i in R
-        @inbounds if rmaxima[i]
-            @inbounds enqueue!(pq, i, output[i])
+        @inbounds current_max = output[i]
+        for Δi in uper_deltaoffest # examine neighborhoods
+            ii = i + Δi
+            if checkbounds(Bool, R, ii) #check that we are in the image
+                @inbounds current_max = max(current_max,output[ii])
+            end
+        end
+        @inbounds output[i] = min(current_max,mask[i])
+    end
+    # backward scan
+    for i in reverse(R)
+        @inbounds current_max = output[i]
+        for Δi in lower_deltaoffest # examine neighborhoods
+            ii = i + Δi
+            if checkbounds(Bool, R, ii) #check that we are in the image
+                @inbounds current_max = max(current_max,output[ii])
+            end
+        end
+        @inbounds output[i] = min(current_max,mask[i])
+        for Δi in lower_deltaoffest # examine neighborhoods
+            ii = i + Δi
+            if checkbounds(Bool, R, ii) #check that we are in the image
+                @inbounds if output[ii]<output[i] && output[ii]<mask[ii]
+                    enqueue!(queue, i)
+                end
+            end
         end
     end
     # Loop until all pixel have been examined 
-    while !isempty(pq)
-        curr_idx, current_priority = dequeue_pair!(pq)
-        # don't examine twice the same point. 
-        @inbounds if visited[curr_idx] && continue 
-        end
-        # mark as visited
-        @inbounds visited[curr_idx] = true
-        # if current priority is greater than actual valuation in output image
-        # update valuation of ouput image -> IE perfom dilation
-        @inbounds if output[curr_idx] < current_priority
-            @inbounds output[curr_idx] = current_priority
-        end
+    while !isempty(queue)
+        curr_idx = dequeue!(queue)
         for Δi in deltaoffsets # examine neighborhoods
             ii = curr_idx + Δi
             if checkbounds(Bool, R, ii) #check that we are in the image
-                @inbounds if visited[ii] && continue # already done ?
+                @inbounds if output[ii] < output[curr_idx] && mask[ii] != output[ii] 
+                    output[ii] = min(output[curr_idx],mask[ii])
+                    enqueue!(queue, ii)
                 end
-                @inbounds newpriority = min(mask[ii], current_priority)
-                pq[ii] =  newpriority
             end
         end
     end
@@ -192,9 +204,8 @@ An array of the same type and shape as the `marker`.
 function overbuild(marker::AbstractArray{T, N}, mask::AbstractArray{T, N}, connectivity::AbstractArray{Bool}) where {T<:ImageCore.NumberLike , N}
     check_image_consistency(mask, marker)
     # WARNING
-    #  The original algorithm compute regional_minima on the Marker
-    #  But this algorithm assume that the marker > mask, this assertion is not always true
-    #  so instead we extract the regional_inima from max(marker,mask)
+    #  The original algorithm assume that the marker > mask, this assertion is not always true
+    #  so instead we extract the start from min(marker,mask)
    
     all(in((1,3)), size(connectivity)) || throw(ArgumentError("connectivity must have size 1 or 3 in each dimension"))
     for d = 1:ndims(connectivity)
@@ -203,40 +214,53 @@ function overbuild(marker::AbstractArray{T, N}, mask::AbstractArray{T, N}, conne
    
     # first operation take the supremum between marker and mask
     output = map(max, marker, mask)
-    # then compute regional_maxima of the Inf(Mask,Marker)
-    rminima = regional_minima(output, connectivity)
-    # declare priority queue, store position of pixel associated with priority 
-    pq = PriorityQueue{CartesianIndex{N}, T}()
+
+    queue = Queue{CartesianIndex{N}}(); 
     deltaoffsets = neighborhood(marker, connectivity)
-    visited = similar(Array{Bool}, axes(marker))
-    fill!(visited, false)
-    # all regional minima are pushed in priority queue
+    uper_deltaoffest = upper_half_neighborhood(marker, connectivity)
+    lower_deltaoffest= lower_half_neighborhood(marker, connectivity)
+
+    # forward scan
     R = CartesianIndices(axes(marker))
     for i in R
-        @inbounds if rminima[i]
-            @inbounds enqueue!(pq, i, output[i])
+        @inbounds current_min = output[i]
+        for Δi in uper_deltaoffest # examine neighborhoods
+            ii = i + Δi
+            if checkbounds(Bool, R, ii) #check that we are in the image
+                @inbounds current_min = min(current_min,output[ii])
+            end
+        end
+        @inbounds output[i] = max(current_min,mask[i])
+    end
+    # backward scan
+    for i in reverse(R)
+        @inbounds current_min = output[i]
+        for Δi in lower_deltaoffest # examine neighborhoods
+            ii = i + Δi
+            if checkbounds(Bool, R, ii) #check that we are in the image
+                @inbounds current_min = min(current_min,output[ii])
+            end
+        end
+        @inbounds output[i] = max(current_min,mask[i])
+        for Δi in lower_deltaoffest # examine neighborhoods
+            ii = i + Δi
+            if checkbounds(Bool, R, ii) #check that we are in the image
+                @inbounds if output[ii]>output[i] && output[ii]>mask[ii]
+                    enqueue!(queue, i)
+                end
+            end
         end
     end
     # Loop until all pixel have been examined 
-    while !isempty(pq)
-        curr_idx, current_priority = dequeue_pair!(pq)
-        # don't examine twice the same point. 
-        @inbounds if visited[curr_idx] && continue 
-        end
-        # mark as visited
-        @inbounds visited[curr_idx] = true
-        # if current priority is lower than actual valuation in output image
-        # update valuation of ouput image -> IE perfom erosion
-        @inbounds if output[curr_idx] > current_priority
-            @inbounds output[curr_idx] = current_priority
-        end
+    while !isempty(queue)
+        curr_idx = dequeue!(queue)
         for Δi in deltaoffsets # examine neighborhoods
             ii = curr_idx + Δi
             if checkbounds(Bool, R, ii) #check that we are in the image
-                @inbounds if visited[ii] && continue# already done ?
+                @inbounds if output[ii] > output[curr_idx] && mask[ii] != output[ii] 
+                    output[ii] = max(output[curr_idx],mask[ii])
+                    enqueue!(queue, ii)
                 end
-                @inbounds newpriority = max(mask[ii], current_priority)
-                pq[ii] =  newpriority
             end
         end
     end
@@ -304,6 +328,44 @@ function neighborhood(A::AbstractArray{T,N}, connectivity::AbstractArray{Bool}) 
     end
     return (offsets...,)   # returning as a tuple allows specialization
 end
+
+function upper_half_neighborhood(A::AbstractArray{T,N}, connectivity::AbstractArray{Bool}) where {T,N}
+    all(in((1,3)), size(connectivity)) || throw(ArgumentError("connectivity must have size 1 or 3 in each dimension"))
+    for d = 1:ndims(connectivity)
+        size(connectivity, d) == 1 || reverse(connectivity; dims=d) == connectivity || throw(ArgumentError("connectivity must be symmetric"))
+    end
+    center = CartesianIndex(map(axes(connectivity)) do ax
+        (first(ax) + last(ax)) ÷ 2
+    end)
+    offsets = CartesianIndex{N}[]
+    for i in CartesianIndices(connectivity)
+        if i == center && break # skip over
+        end
+        if connectivity[i]
+            push!(offsets, i - center)
+        end
+    end
+    return (offsets...,)   # returning as a tuple allows specialization
+end 
+
+function lower_half_neighborhood(A::AbstractArray{T,N}, connectivity::AbstractArray{Bool}) where {T,N}
+    all(in((1,3)), size(connectivity)) || throw(ArgumentError("connectivity must have size 1 or 3 in each dimension"))
+    for d = 1:ndims(connectivity)
+        size(connectivity, d) == 1 || reverse(connectivity; dims=d) == connectivity || throw(ArgumentError("connectivity must be symmetric"))
+    end
+    center = CartesianIndex(map(axes(connectivity)) do ax
+        (first(ax) + last(ax)) ÷ 2
+    end)
+    offsets = CartesianIndex{N}[]
+    for i in reverse(CartesianIndices(connectivity))
+        if i == center && break # skip over
+        end
+        if connectivity[i]
+            push!(offsets, i - center)
+        end
+    end
+    return (offsets...,)   # returning as a tuple allows specialization
+end 
 
 #faster than local_minima/maxima from maxtree
 function extract_regional_extrema(image::AbstractArray{T, N}, connectivity::AbstractArray{Bool}, CompareOperator = Base.isgreater, EquivalenceOperator = Base.isequal ) where {T<:ImageCore.NumberLike , N}
