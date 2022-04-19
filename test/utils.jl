0c91db78f2812a33c10189c8b0f29ead5ca30641
Johnny Chen <johnnychen94@hotmail.com>
2022-04-19 19:05:18 +0800

    fix test

--- test/utils.jl
+++ test/utils.jl
@@ -31,7 +31,11 @@
     @test ref == _build((1, 3); square=false) == _build((1, 3), 2; square=false)
 
     # N=3
-    out = @inferred _build(2, Val(3))
+    out = if VERSION >= v"1.6"
+        @inferred _build(2, Val(3))
+    else
+        _build(2, Val(3))
+    end
     ref = cat(
         Bool[0 0 0 0 0; 0 0 0 0 0; 0 0 1 0 0; 0 0 0 0 0; 0 0 0 0 0],
         Bool[0 0 0 0 0; 0 1 1 1 0; 0 1 1 1 0; 0 1 1 1 0; 0 0 0 0 0],
@@ -44,7 +48,7 @@
     @test out == ref
 
     # not type-inferable
-    err = ErrorException("return type BitMatrix does not match inferred return type Any")
+    err = ErrorException("return type $(BitMatrix) does not match inferred return type Any")
     @test_throws err @inferred _build(1, 2)
     @test _build(1, 2) == _build(1, Val(2))
 
@@ -69,7 +73,12 @@ end
     @test typeof(out) == BitMatrix
     @test out == ref
 
+    # not guaranteed to be type-inferable
+    @test _build(:C4, 2) == _build(:C4, Val(2))
+
     # exceptions
     err = ErrorException("unknown connectivity alias for dimension 2: null")
     @test_throws err _build(:null)
+    err = ErrorException("unsupported dimension 999")
+    @test_throws err _build(:null, Val(999))
 end
