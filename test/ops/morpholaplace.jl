c6c1c51bead8dab03ed22b076df1b4b6cf4db123
Johnny Chen <johnnychen94@hotmail.com>
2022-05-20 17:16:38 +0800

    morpholaplace: add keyword `r` and rewrite tests

--- test/ops/morpholaplace.jl
+++ test/ops/morpholaplace.jl
@@ -1,13 +1,30 @@
-@testset "Morphological Laplacian" begin
-    A = zeros(13, 13)
-    A[5:9, 5:9] .= 1
-    Ao = morpholaplace(A)
-    @test morpholaplace(A; dims=1:2) == Ao
-    Ae = zeros(13, 13)
-    Ae[4:10, 4:10] .= 1
-    Ae[5:9, 5:9] .= -1
-    Ae[6:8, 6:8] .= 0
-    @test Ao == Ae
-    Aee = dilate(A) + erode(A) - 2A
-    @test Aee == Ae
+morpholaplace_ref(img, dims, r) = morpholaplace_ref(img, strel_box(img, dims; r))
+morpholaplace_ref(img, se) = float.(dilate(img, se)) .+ float.(erode(img, se)) .- 2 .* float.(img)
+
+@testset "morpholaplace" begin
+    test_ranges = [Bool, 1:10, Float64, Gray{N0f8}, Gray{Float64}]
+    for T in test_ranges
+        for N in (1, 2, 3)
+            sz = ntuple(_ -> 32, N)
+            img = rand(T, sz...)
+
+            out = morpholaplace(img)
+            @test out == morpholaplace(img, strel_box(img, ntuple(identity, N); r=1))
+            @test out == morpholaplace_ref(img, ntuple(identity, N), 1)
+
+            @test morpholaplace(img; dims=(1,), r=2) == morpholaplace_ref(img, (1,), 2)
+
+            se = centered(rand(Bool, ntuple(_ -> 3, N)))
+            @test morpholaplace(img, se) == morpholaplace_ref(img, se)
+        end
+    end
+
+    # TODO(johnnychen94): support this
+    # img = rand(1:5, 7, 7)
+    # out = similar(img)
+    # morpholaplace!(out, img, similar(img))
+    # @test out == morpholaplace(img)
+
+    img = rand(RGB, 7, 7)
+    @test_throws ArgumentError("color image is not supported") morpholaplace(img)
 end
